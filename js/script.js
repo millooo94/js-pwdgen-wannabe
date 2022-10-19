9e7ad380d232bd73e3e69cd9e01772b1167fcf83
millooo94 <camillomusmeciunict@gmail.com>
2022-10-19 14:45:27 +0200

    Add all

---
+++ js/script.js
@@ -0,0 +1,5 @@
+let name = prompt ('Qual è il tuo nome?')
+let surname = prompt ('Qual è il tuo cognome')
+let favoriteColor = prompt ('Qual è il tuo colore preferito?')
+let valor = name + surname + favoriteColor + '22'
+document.getElementById ('password').innerHTML = valor

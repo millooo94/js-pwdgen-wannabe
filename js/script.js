dd59557e4b41e1305542470db6ef8e8fb6528dbf
millooo94 <camillomusmeciunict@gmail.com>
2022-10-19 19:14:30 +0200

    Refactor all

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 let name = prompt ('Qual è il tuo nome?')
 let surname = prompt ('Qual è il tuo cognome')
 let favoriteColor = prompt ('Qual è il tuo colore preferito?')
-let valor = name + surname + favoriteColor + '22'
+let valor = `${name}${surname}${favoriteColor}22`
+// let valor = name + surname + favoriteColor + "22"
 document.getElementById ('password').innerHTML = valor
+
